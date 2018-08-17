2f5f0632113b1b81c1e4301df762868a2948a7dc
onlyjyf <onlyjyf@nobook.com>
2018-08-17 17:37:54 +0800

    调整api

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 (function() {
-	var reg = /http:\/\/(.*):/;
-	var result = reg.exec(document.location.href);
-	var ws = new WebSocket('ws://' + result[1] + ':8081');
-	ws.addEventListener('message', function(messageEvt) {
-	    console.log(messageEvt.data);
-	});
-	window.__temp__ws = ws;
+	function exec() {
+		return new Promise(function(resolve, reject) {
+			var reg = /http:\/\/(.*):/;
+			var timeoutID;
+			var result = reg.exec(document.location.href);
+			var ws = new WebSocket('ws://' + result[1] + ':8081');
+			ws.addEventListener('open', function (event) {
+				clearTimeout(timeoutID);
+				resolve();
+			});
+			ws.addEventListener('message', function(messageEvt) {
+			    console.log(messageEvt.data);
+			});
+
+			// 5秒超时
+			timeoutID = setTimeout(function() {
+				reject();
+			}, 5000);
+			window.__temp__ws = ws;
+		});
+	}
+
+	if (window) {
+		window.initializeConnection = exec;
+	}
 })();
